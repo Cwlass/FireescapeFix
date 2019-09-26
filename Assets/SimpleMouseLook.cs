9f6b5c2cfbc50c6e474bdabef3c4e45d4a9d96a7
David Ferns <Cwlass@gmail.com>
2019-09-26 02:50:01 +0200

    Beginning of pc version dev.

---
+++ Assets/SimpleMouseLook.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleMouseLook : MonoBehaviour
+{
+
+    Vector2 rotation = Vector2.zero;
+    [SerializeField] private float speed = 3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rotation.y += Input.GetAxis("Mouse X");
+        rotation.x += Input.GetAxis("Mouse Y");
+        rotation.x = Mathf.Clamp(rotation.x, -15f, 15f);
+        transform.eulerAngles = new Vector2(0, rotation.y) * speed;
+        Camera.main.transform.localRotation = Quaternion.Euler(rotation.x * -speed, 0, 0);
+    }
+}
---
+++ Assets/VRCheck.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VR;
+using Valve.VR;
+
+public class VRCheck : MonoBehaviour
+{
+    [SerializeField] GameObject steamVR;
+    [SerializeField] GameObject cameraRig;
+    [SerializeField] GameObject nonVRRig;
+    private void Awake()
+    {
+        if (!SteamVR.active)
+        {
+            steamVR.SetActive(false);
+            cameraRig.SetActive(false);
+            nonVRRig.SetActive(true);
+        }
+    }
+
+}
