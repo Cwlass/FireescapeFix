7b5b9da98b8edd50ea49123471fdb0e9d99933a6
David <Cwlass@gmail.com>
2019-11-05 19:49:39 +0100

    FPS Camera working. (no jumping)

--- Assets/NonVRController.cs
+++ Assets/NonVRController.cs
@@ -1,18 +1,41 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
+
+
 
 public class NonVRController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    public CharacterController controller;
+
+    public float speed = 12f;
+    public float gravity = -9.81f;
+
+    public Transform groudCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    Vector3 velocity;
+    bool isGrounded;
+
+    private void Update()
     {
-        
+        isGrounded = Physics.CheckSphere(groudCheck.position, groundDistance, groundMask);
+
+        if(isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * speed * Time.deltaTime);
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
     }
 }
--- Assets/SimpleMouseLook.cs
+++ Assets/SimpleMouseLook.cs
@@ -10,16 +10,15 @@ public class SimpleMouseLook : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cursor.visible = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         rotation.y += Input.GetAxis("Mouse X");
-        rotation.x += Input.GetAxis("Mouse Y");
+        rotation.x += -Input.GetAxis("Mouse Y");
         rotation.x = Mathf.Clamp(rotation.x, -15f, 15f);
-        transform.eulerAngles = new Vector2(0, rotation.y) * speed;
-        Camera.main.transform.localRotation = Quaternion.Euler(rotation.x * -speed, 0, 0);
+        transform.eulerAngles = (Vector2)rotation * speed;
     }
 }
